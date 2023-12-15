9561626f29765197fcb7bfa9c39e77bb9210b34b
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2023-12-15 12:44:29 -0800

    lets keep going

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LinkedLists {
+    class Program {
+        static void Main(string [] args) {
+
+            var najib = new Node();
+            najib.data= 25;
+            najib.DisplayNode();
+
+            var pink = new Node();
+            pink.data= 20;
+            pink.DisplayNode();
+
+            var azhar = new Node();
+            azhar.data = 21;
+            azhar.DisplayNode();
+
+            najib.Next=pink;
+            pink.Next = azhar;
+            azhar.Next= null;
+        }
+    }
+
+    class Node {
+        public int data{get; set;}
+        public Node? Next {get; set;}
+        public void DisplayNode()
+        {
+             Console.WriteLine(data);
+        }
+
+    }
+
+
+
+
+}
