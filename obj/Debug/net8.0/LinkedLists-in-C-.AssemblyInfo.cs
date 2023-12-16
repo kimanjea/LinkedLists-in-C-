642421a9e39ee29096e83acdaf22b29f075f8cd3
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2023-12-16 00:15:08 -0800

    who am i to give up

--- obj/Debug/net8.0/LinkedLists-in-C-.AssemblyInfo.cs
+++ obj/Debug/net8.0/LinkedLists-in-C-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinkedLists-in-C-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff978a9ebf33aa26a29a796fb03132eb0710efe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinkedLists-in-C-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinkedLists-in-C-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
